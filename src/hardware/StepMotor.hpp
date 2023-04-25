837e988fbefc1c82cb35d8cd6e5c9b3698dc2d6c
Jetiou1312 <arnaud_quinet@hotmail.com>
2023-04-25 21:03:26 +0200

    src/hardware/StepMotor.hpp

--- src/hardware/StepMotor.hpp
+++ src/hardware/StepMotor.hpp
@@ -7,7 +7,36 @@ class StepMotor : public Hardware
     private :
         const int dirPin;
         const int stepPin;
+        const int stepsPerRevolution;
     public :
-        StepMotor(int _dirPin,int _stepPin);
+        StepMotor(int _dirPin,int _stepPin,int _stepsPerRevolution) : dirPin(_dirPin), stepPin(_stepPin), stepsPerRevolution(_stepsPerRevolution)
+        {}
+
+        void StepMotor::setup(void)
+        {
+            pinMode(dirPin,OUTPUT);
+            pinMode(stepPin,OUTPUT);
+        }
+
+        void StepMotor::direction(bool sens)
+        {
+            if(sens)
+            {
+                digitalWrite(dirPin,HIGH);
+            }
+            else
+            {
+                digitalWrite(dirPin,LOW);
+            }
+        }
+
+        void StepMotor::run(int rapidity)
+        {
+            digitalWrite(stepPin,HIGH);
+            delayMicroseconds(rapidity);
+            digitalWrite(stepPin,LOW);
+            delayMicroseconds(rapidity);
+        }
         
 };
+
