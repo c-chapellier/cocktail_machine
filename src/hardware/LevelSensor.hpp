226a091a49fe59af006b625d8467b8256ea2d017
Jetiou1312 <arnaud_quinet@hotmail.com>
2023-04-25 12:33:46 +0200

    src/hardware/LevelSensor.hpp

---
+++ src/hardware/LevelSensor.hpp
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#include "global.hpp"
+
+class LevelSensor : public Hardware
+{
+    private : 
+        const int pin ;
+    public :
+        LevelSensor( int pin) : pin(pin) 
+        {};
+    void LevelSensor::setup(void)
+    {
+        pinMode(pin,OUTPUT);
+    }
+
+    bool LevelSensor::LiquidDetected(void)
+
+    {
+        if(digitalRead(pin)==0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+};
